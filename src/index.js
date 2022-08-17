deccfdc64c249f1041585e7a257fd7e7424cc699
Phuoc Tran <tranhuyphuoc1999@gmail.com>
2022-08-17 14:35:45 +0700

    config something

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/view'))
+
+app.get('/trang-chu', (req, res) => res.render('home'))
+
+app.get('/news', (req, res) => res.render('news'))
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
