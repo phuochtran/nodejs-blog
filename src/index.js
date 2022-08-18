f09028a16015c12df69adbd64a19a2d8b5f3f82f
Phuoc Tran <tranhuyphuoc1999@gmail.com>
2022-08-18 11:02:25 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const { readSync } = require('fs');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
 
 // Template engine
-app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resource/view'))
+app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/view'));
 
-app.get('/trang-chu', (req, res) => res.render('home'))
+route(app);
 
-app.get('/news', (req, res) => res.render('news'))
+// app.get('/', (req, res) => res.render('home'))
 
+// app.get('/news', (req, res) => res.render('news'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// app.get('/search', (req, res) => res.render('search'))
+
+                    app.listen(port, 
+                        
+                        () =>
+                console.log(`
+  
+                Example app listening at http://localhost:${port}`),
+);
